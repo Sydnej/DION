b9543f6a636f8be9810165da80fcb8344f5ba4d4
Sydnej <sydnej.w@gmail.com>
2015-06-16 01:42:32 +0200

    new

---
+++ PhotoSMS/PhotoSMS/Image.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoSMS
+{
+    class Image
+    {
+
+        string name;
+        string date;
+        string tags;
+        string link;
+        float size;
+
+        void setName(string name) { this.name = name; }
+        string getName() { return this.name; }
+
+        void setDate(string dat) { this.date = dat; }
+        string getDate() { return this.date; }
+
+        void setTags(string tag) { this.tags = tag; }
+        string getTags() { return this.tags; }
+
+        void setLink(string lin) { this.link = lin; }
+        string getLink() { return this.link; }
+
+        void setSize(float siz) { this.size = siz; }
+        float getSize() { return this.size; }
+
+
+    }
+}
---
+++ PhotoSMS/PhotoSMS/Users.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoSMS
+{
+    class Users
+    {
+
+        string name;
+        string surname;
+        string login;
+        string password;
+        string email;
+
+        void setName(string name) { this.name = name; }
+        string getName() { return this.name; }
+
+        void setSurname(string sname) { this.surname = sname; }
+        string getSurname() { return this.surname; }
+
+        void setLogin(string log) { this.login = log; }
+        string getLogin() { return this.login; }
+
+        void setPasword(string psswd) { this.password = psswd; }
+        string getPassword() { return this.password; }
+
+        void setEmail(string em) { this.email = em; }
+        string getEmail() { return this.email; }
+
+    }
+}
